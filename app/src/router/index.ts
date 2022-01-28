c4fde3e6a206ca6620d1ac652571abda5161b674
GeneTv <contact@genetv.net>
2022-01-28 10:02:14 +0100

    Fixed router issues

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import routing from './routing';
 import Home from '../views/Home.vue';
+import Login from '../views/Login.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
-    component: Home,
+    component: Home
   },
   {
     path: '/about',
@@ -13,13 +15,20 @@ const routes: Array<RouteRecordRaw> = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  }
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes,
+  routes
 });
+
+router.beforeEach(routing); // eslint-disable-line
 
 export default router;
