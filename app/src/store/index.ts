1881946acda7408faa2d523b2ca0b02b42f156cf
GeneTv <contact@genetv.net>
2022-01-28 10:04:20 +0100

    Implemented login in store

--- app/src/store/index.ts
+++ app/src/store/index.ts
@@ -2,11 +2,19 @@ import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    isLoggedIn: false
   },
   mutations: {
+    LOGIN(state) {
+      state.isLoggedIn = true;
+    }
   },
   actions: {
+    loginUser(context, credentials) {
+      context.commit('LOGIN');
+    }
   },
-  modules: {
-  },
+  getters: {
+    isLoggedIn: (state) => state.isLoggedIn
+  }
 });
